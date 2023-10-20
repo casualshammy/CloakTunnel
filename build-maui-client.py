9e3dca381ffaa9ebaf88a467b1a36a6afd3e044a
casualshammy <axio@axio.name>
2023-10-20 20:00:48 +0100

    feature(build): initial build scripts

---
+++ build-maui-client.py
@@ -0,0 +1,67 @@
+import os
+import re
+import shutil
+import build_common.packages
+import build_common.git as git
+import argparse
+
+signingPassword = os.environ['ANDROID_SIGNING_KEY_PASSWORD']
+
+sourceDirName = "SlowUdpPipe.MauiClient"
+
+argParser = argparse.ArgumentParser()
+argParser.add_argument('--framework', type=str, default= "net6.0-android", required=False, help='Target framework of server')
+args = argParser.parse_args()
+framework: str = args.framework
+
+artifactsDir = os.path.join(os.getcwd(), "artifacts")
+if (not os.path.isdir(artifactsDir)):
+    os.makedirs(artifactsDir)
+
+outputDir = os.path.join(os.getcwd(), "output")
+if (os.path.isdir(outputDir)):
+    shutil.rmtree(outputDir, ignore_errors=True)
+
+outputFileRegex = ""
+if (framework.endswith("android")):
+    outputFileRegex = r"-(Signed)\.apk$|-(Signed)\.aab$"
+
+if (outputFileRegex == ""):
+    raise FileNotFoundError(f"Can't find pkg format for framework '{framework}'")
+
+branch = git.get_current_branch()
+commitIndex = git.get_last_commit_index()
+version = f"{branch}.{commitIndex}"
+
+print(f"===========================================", flush=True)
+print(f"Output folder: '{outputDir}'", flush=True)
+print(f"===========================================", flush=True)
+
+print(f"===========================================", flush=True)
+print(f"Adjusting csproj version: '{version}'", flush=True)
+print(f"===========================================", flush=True)
+build_common.packages.adjust_csproj_version(os.path.join(os.getcwd(), sourceDirName), version, "ApplicationDisplayVersion")
+build_common.packages.adjust_csproj_version(os.path.join(os.getcwd(), sourceDirName), str(commitIndex), "ApplicationVersion")
+
+print(f"===========================================", flush=True)
+print(f"Compiling client for framework '{framework}'...", flush=True)
+print(f"===========================================", flush=True)
+build_common.packages.callThrowIfError("dotnet workload install maui")
+build_common.packages.callThrowIfError(f"dotnet publish {sourceDirName} -c Release -p:AndroidSigningKeyPass={signingPassword} -p:AndroidSigningStorePass={signingPassword} -f {framework} -o \"{outputDir}\"")
+
+print(f"===========================================", flush=True)
+print(f"Creating pkg...", flush=True)
+print(f"===========================================", flush=True)
+for entry in os.listdir(outputDir):
+    entryPath = os.path.join(outputDir, entry)
+    if (os.path.isfile(entryPath) ):
+        match = re.search(outputFileRegex, entryPath)
+        if (match != None):
+            newName = entry.replace(match.group(1) or match.group(2), version)
+            shutil.move(entryPath, os.path.join(artifactsDir, newName))
+
+print(f"===========================================", flush=True)
+print(f"Done!", flush=True)
+print(f"===========================================", flush=True)
+
+git.create_tag_and_push(version)
---
+++ build-server-client.py
@@ -0,0 +1,79 @@
+import os
+import shutil
+import zipfile
+from build_common import utils
+from build_common import packages
+from build_common import git
+import argparse
+
+sourceDirNameServer = "SlowUdpPipe.Server"
+sourceDirNameClient = "SlowUdpPipe.Client"
+
+argParser = argparse.ArgumentParser()
+argParser.add_argument('--platform', type=str, default= "win-x64", required=False, help='Target platfrom of server')
+args = argParser.parse_args()
+platform = args.platform
+
+artifactsDir = os.path.join(os.getcwd(), "artifacts")
+if (not os.path.isdir(artifactsDir)):
+    os.makedirs(artifactsDir)
+outputDir = os.path.join(os.getcwd(), "output")
+if (os.path.isdir(outputDir)):
+    shutil.rmtree(outputDir, ignore_errors=True)
+
+pkgFileServer = os.path.join(artifactsDir, f"server-{platform}.zip")
+if (os.path.isfile(pkgFileServer)):
+    os.remove(pkgFileServer)
+
+pkgFileClient = os.path.join(artifactsDir, f"client-{platform}.zip")
+if (os.path.isfile(pkgFileClient)):
+    os.remove(pkgFileClient)
+
+branch = git.get_current_branch()
+commitIndex = git.get_last_commit_index()
+version = f"{branch}.{commitIndex}"
+
+print(f"===========================================", flush=True)
+print(f"Output folder: '{outputDir}'", flush=True)
+print(f"===========================================", flush=True)
+
+print(f"===========================================", flush=True)
+print(f"Compiling server for platform '{platform}'...", flush=True)
+print(f"Version: '{version}'", flush=True)
+print(f"===========================================", flush=True)
+serverOutputDir = os.path.join(outputDir, "server")
+packages.adjust_csproj_version(os.path.join(os.getcwd(), sourceDirNameServer), version)
+utils.callThrowIfError(f"dotnet publish {sourceDirNameServer} -c release -r {platform} --self-contained -o \"{serverOutputDir}\"")
+
+print(f"===========================================", flush=True)
+print(f"Compiling client for platform '{platform}'...", flush=True)
+print(f"Version: '{version}'", flush=True)
+print(f"===========================================", flush=True)
+clientOutputDir = os.path.join(outputDir, "client")
+packages.adjust_csproj_version(os.path.join(os.getcwd(), sourceDirNameClient), version)
+utils.callThrowIfError(f"dotnet publish {sourceDirNameClient} -c release -r {platform} --self-contained -o \"{clientOutputDir}\"")
+
+print(f"===========================================", flush=True)
+print(f"Creating server pkg...", flush=True)
+print(f"===========================================", flush=True)
+with zipfile.ZipFile(pkgFileServer, 'w', zipfile.ZIP_DEFLATED) as pkgZipFile:
+    for root, _, files in os.walk(serverOutputDir):
+        for file in files:
+            filePath = os.path.join(root, file)
+            pkgZipFile.write(filePath, os.path.relpath(filePath, serverOutputDir))
+
+print(f"===========================================", flush=True)
+print(f"Creating client pkg...", flush=True)
+print(f"===========================================", flush=True)
+with zipfile.ZipFile(pkgFileClient, 'w', zipfile.ZIP_DEFLATED) as pkgZipFile:
+    for root, _, files in os.walk(clientOutputDir):
+        for file in files:
+            filePath = os.path.join(root, file)
+            pkgZipFile.write(filePath, os.path.relpath(filePath, clientOutputDir))
+
+print(f"===========================================", flush=True)
+print(f"Done! Server package file is '{pkgFileServer}'", flush=True)
+print(f"Done! Client package file is '{pkgFileClient}'", flush=True)
+print(f"===========================================", flush=True)
+
+git.create_tag_and_push(version)
